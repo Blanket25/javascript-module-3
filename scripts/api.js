fd47512f71f38c9a7328ece4c4daff320e09443a
Blanket25 <bianca_inga@hotmail.com>
2021-09-06 10:36:52 +0200

    remade with modules

--- scripts/api.js
+++ scripts/api.js
@@ -1,8 +1,19 @@
-async function getEpisodes(page) {
-    const response = await fetch(`https://rickandmortyapi.com/api/episode?page=${page}`);
-    const result = await response.json();
-    return result;
+async function getListOfEpisodes(page) {
+  const url = `https://rickandmortyapi.com/api/episode?page=${page}`;
+  const json = await getUrl(url);
+  return json.results;
 }
 
+async function getEpisode(episode) {
+  const url = `https://rickandmortyapi.com/api/episode/${episode}`;
+  return getUrl(url);
+}
 
-export {getEpisodes};
+async function getUrl(url) {
+  const res = await fetch(url);
+  const json = await res.json();
+
+  return json;
+}
+
+export { getListOfEpisodes, getEpisode, getUrl };
